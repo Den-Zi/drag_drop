3209b962f979e74211bfbcb5aa8e08314f8f2933
Den_Zi <dkartoshkina@bk.ru>
2023-07-20 00:20:35 +0500

    Закончиен Drag&Drop

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ console.log('файл подлючен')
 const dragItem = document.querySelectorAll('.dragItem');
 const dropZone = document.querySelectorAll('.dropZone');
 
+let draggedItem = null;
+let droppedItem = null; // переменная над которой соверешн сброс 
+
 dragItem.forEach(dragItem => {
-    dragItem.addEventListener('dragstart', handlerDagrstart)
-    dragItem.addEventListener('dragend', handlerDagrend)
-    dragItem.addEventListener('drag', handlerDag)
+    dragItem.addEventListener('dragstart', handlerDagrstart);
+    dragItem.addEventListener('dragend', handlerDagrend);
+    dragItem.addEventListener('drag', handlerDag);
+    dragItem.addEventListener('dragenter', () => {   //Прослушка над элементом 
+        if (dragItem !== droppedItem) { // если сбрасывание происходит нед над самим объектом будем его запоминать
+            droppedItem = dragItem;
+        }
+    });
+    dragItem.addEventListener('dragleave', () => {
+        droppedItem = null;
+    });
+
 });
 
 dropZone.forEach(dropZone => {
@@ -16,11 +28,11 @@ dropZone.forEach(dropZone => {
     dropZone.addEventListener('drop', handlerDrop)
 })
 
-let draggedItem = null;
 
 
 
-function handlerDagrstart (event) {
+
+function handlerDagrstart(event) {
     // console.log("dragstart", this);
     this.classList.add('dragItem--active');
     draggedItem = this;
@@ -28,35 +40,51 @@ function handlerDagrstart (event) {
     // event.dataTransfer.setData('dragItem', this.dataset.item);
 }
 
-function handlerDagrend (event) {
+function handlerDagrend(event) {
     // console.log("dragend", this)
     this.classList.remove('dragItem--active');
     draggedItem = null;
 }
 
-function handlerDag () {
+function handlerDag() {
     // console.log('drag')
 }
 
 
-function handlerDragenter (event) {
+function handlerDragenter(event) {
     event.preventDefault();
     // console.log('Dragover')
     this.classList.add('dropZone--active');
 }
 
-function handlerDragleave (event) {
+function handlerDragleave(event) {
     // console.log('Dragleave')
     this.classList.remove('dropZone--active');
 }
 
-function handlerDragover (event) {
+function handlerDragover(event) {
     event.preventDefault();
     // console.log('Dragover')
 }
 
-function handlerDrop (event) {
+function handlerDrop(event) {
     // const dragFlag = event.dataTransfer.getData('dragItem');
     // const dragItem = document.querySelector(`[data-item="${dragFlag}"]`);
-    this.append(draggedItem);
+    if (droppedItem) {
+        if (droppedItem.parentElement === draggedItem.parentElement) {
+            // console.log('Общий родительский элемент')
+            const children = Array.from(droppedItem.parentElement.children);
+            const draggedIndex = children.indexOf(draggedItem);
+            const droppedIndex = children.indexOf(droppedItem);
+
+            if (draggedIndex > droppedIndex) {
+                draggedItem.parentElement.insertBefore(draggedItem, droppedItem);
+            } else {
+                draggedItem.parentElement.insertBefore(draggedItem, droppedItem.nextElementSibling);
+            };
+        }
+    } else {
+        this.append(draggedItem);
+    }
+    dropZone.forEach((x) => x.classList.remove('dropZone--active'));
 }
